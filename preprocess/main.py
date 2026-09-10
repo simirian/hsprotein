d74d49100c299f362959e90fd107c1c4096ce533
simirian <simirian5813@gmail.com>
2026-09-10 14:15:06 -0400

    feat(preproc): fetch from PISCES output
    Improved the fetch/preprocess script so it can fetch from PISCES output
    by using the first four non-blank characters as PDB ID, then everything
    after that until whitespace as a chain id. The specific chain ends up in
    a file with `_fchain`.

--- preprocess/main.py
+++ preprocess/main.py
@@ -6,15 +6,16 @@ import os
 import sys
 from argparse import ArgumentParser
 
-import hydride
 import numpy as np
 from biotite.database.rcsb import fetch
-from biotite.structure import AtomArrayStack, filter_amino_acids, stack
+from biotite.structure import AtomArrayStack, filter_amino_acids
 from biotite.structure.io import load_structure, save_structure
+from hydride import add_hydrogen, relax_hydrogen  # type: ignore
 
 infile: str
 outdir: str
 override: bool = False
+skip: int = 0
 
 
 def help():
@@ -55,57 +56,78 @@ def fetch_files() -> list[str]:
     """Fetches the list of files from RCSB PDB. Requires internet."""
     pdbids: list[str]
     with open(infile) as f:
-        pdbids = [id.strip() for id in f]
+        pdbids = [id.strip()[0:4] for id in f.readlines()[skip:]]
 
     return fetch(pdbids, "cif", outdir, verbose=True, overwrite=override)
 
 
 def process_files(files: list[str]):
     """Processes the fetched files."""
+    with open(infile) as f:
+        chains: dict[str, str] = {
+            line.strip()[0:4]: line.strip().split(" ")[0][4:]
+            for line in f.readlines()[skip:]
+        }
     for file in files:
-        file = os.path.normpath(file)
-        name, ext = os.path.splitext(file)
-
-        noalt_file = name + "_noalt" + ext
-        aaonly_file = name + "_aaonly" + ext
-        withh_file = name + "_withh" + ext
-
-        if (
-            os.path.exists(noalt_file)
-            and os.path.exists(aaonly_file)
-            and os.path.exists(withh_file)
-            and not override
-        ):
-            continue
+        pdbid = os.path.splitext(os.path.basename(file))[0]
+        name, ext = os.path.splitext(os.path.normpath(file))
+        print("processing", pdbid)
 
         # loading the file will automatically select only the first altloc of each atom
         # bonds and charge are needed for hydride to function properly
-        noalt = load_structure(file, include_bonds=True, extra_fields=["charge"])
-        save_structure(noalt_file, noalt)
-
-        # strip everything but the protein
-        aaonly = noalt[filter_amino_acids(noalt)]
-        save_structure(aaonly_file, aaonly)
-
-        # add hydrogen atoms
-        if np.any(aaonly.atom_name == "H"):
-            save_structure(withh_file, aaonly)
+        noalt_file = name + "_noalt" + ext
+        if not override and os.path.exists(noalt_file):
+            noalt = None
         else:
-            if isinstance(aaonly, AtomArrayStack):
-                withh = []
-                for i in range(aaonly.stack_depth()):
-                    array = aaonly[i]
-                    withh.append(hydride.add_hydrogen(array))
-                    withh[-1].coord = hydride.relax.relax_hydrogen(withh[-1])
-                save_structure(withh_file, stack(withh))
+            noalt = load_structure(file, include_bonds=True, extra_fields=["charge"])
+            if isinstance(noalt, AtomArrayStack):
+                noalt = noalt.get_array(0)
+                save_structure(noalt_file, noalt)
             else:
-                withh, _ = hydride.add_hydrogen(aaonly)
-                withh.coord = hydride.relax.relax_hydrogen(withh)
-                save_structure(withh_file, withh)
+                save_structure(noalt_file, noalt)
+
+        aaonly_file = name + "_aaonly" + ext
+        if not override and os.path.exists(aaonly_file):
+            aaonly = None
+        else:
+            noalt = noalt or load_structure(
+                noalt_file, include_bonds=True, extra_fields=["charge"]
+            )
+            aaonly = noalt[filter_amino_acids(noalt)]
+            save_structure(aaonly_file, aaonly)
+
+        withh_file = name + "_withh" + ext
+        if not override and os.path.exists(withh_file):
+            withh = None
+        else:
+            aaonly = aaonly or load_structure(
+                aaonly_file, include_bonds=True, extra_fields=["charge"]
+            )
+            if np.any(aaonly.element == "H"):
+                withh = aaonly
+            else:
+                print("  adding H")
+                withh, _ = add_hydrogen(aaonly)
+                print("  added, relaxing H")
+                withh.coord = relax_hydrogen(withh)
+                print("  relaxed H")
+            save_structure(withh_file, withh)
+
+        fchain_file = name + "_fchain" + ext
+        if not override and os.path.exists(fchain_file):
+            pass
+        else:
+            withh = withh or load_structure(withh_file)
+            fchain = (
+                withh[withh.chain_id == chains[pdbid]]
+                if (pdbid in chains and chains[pdbid] != "")
+                else withh
+            )
+            save_structure(fchain_file, fchain)
 
 
 def main():
-    global infile, outdir, override
+    global infile, outdir, override, skip
 
     parser = ArgumentParser()
     parser.add_argument(
@@ -115,11 +137,13 @@ def main():
         "outdir", nargs=1, help="The output directory to place files in."
     )
     parser.add_argument("-o", "--override", action="store_true")
+    parser.add_argument("-s", "--skip", action="store", type=int, default=0)
     args = parser.parse_args()
 
     infile = args.infile[0]
     outdir = args.outdir[0]
     override = args.override
+    skip = args.skip
 
     if not validate_io():
         sys.exit(2)
