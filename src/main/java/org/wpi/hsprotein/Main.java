a5a3f51da74c1bd9d9a73bab4079d913a97852f3
simirian <simirian5813@gmail.com>
2026-09-12 18:32:10 -0400

    feat!: visualizer DO NOT MERGE
    Adds a visualizer for dihedral angles, could probably be improved but
    this works good enough. This branch/commit steamrolls the main function
    in order to do the visualizations and ignores most of the rest of the
    code. It's in the github for the sake of transparency.
    
    DO NOT MERGE

--- src/main/java/org/wpi/hsprotein/Main.java
+++ src/main/java/org/wpi/hsprotein/Main.java
@@ -1,106 +1,64 @@
 package org.wpi.hsprotein;
+import org.biojava.nbio.structure.Structure;
+import org.biojava.nbio.structure.gui.BiojavaJmol;
+import org.biojava.nbio.structure.io.CifFileReader;
 import org.wpi.hsprotein.filemanager.ProteinManager;
-import org.wpi.hsprotein.rotations.Rotator;
-
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-import org.biojava.nbio.structure.Chain;
-import org.biojava.nbio.structure.Group;
-import org.biojava.nbio.structure.Atom;
-import org.biojava.nbio.structure.BondImpl;
+import org.wpi.hsprotein.rotations.DihedralAngle;
 
 class Main {
-	/** 
-	 * Alerts the user of improper command line arguments in a consistent, formatted way.
-	 * <b>Exits the program.<b>
-	 * @param errorMessage the error message to display to the user
-	 */
-	private static void handleImproperUserInput(String errorMessage) {
-		System.out.println("\n\033[31m" + "Error: " + errorMessage + "\033[0m");
-		System.out.println("See README for example arguments and proper usage.\n");
-		System.exit(1);
-	}
+  public static void main(String[] args) {
+    if (args.length < 3) {
+      System.out.println("Needs more args!\ncmd FILE RESIDUE NANGLE");
+      return;
+    }
+    String filepath = args[0];
 
-	public static void main(String[] args) {
-		// Assign command line args to named variables for readability
-		String pathToInputFileDirectory = args[0];
-		String inputFileName = args[1];
-		String filepath = pathToInputFileDirectory + "/" + inputFileName;
-		
-		// Check if input directory exists
-		if (!Files.exists(Paths.get(args[0])) || !Files.isDirectory(Paths.get(args[0]))) {
-			handleImproperUserInput("Invalid input directory path");
-		}
-		// Check if input file exists
-		if (!(new File(filepath).isFile())) {
-			handleImproperUserInput("Invalid input file");
-		}
-		// Check if Residue ID is a valid number
-		int residueID;
-		try {
-			residueID = Integer.parseInt(args[2]);
-		}
-		catch (NumberFormatException e) {
-			handleImproperUserInput("Invalid ResidueID (not a number)");
-		}
-		// Check if output directory exists
-		String pathToOutputFileDirectory = args[3];
-		if (!Files.exists(Paths.get(args[3])) || !Files.isDirectory(Paths.get(args[3]))) {
-			handleImproperUserInput("Invalid output directory path");
-		}
-		// Check if only-dipeptide tag is a valid boolean (1 / 0)
-		Boolean dipeptideOnlyTag;
-		try {
-			int tempTag = Integer.parseInt(args[4]);
-			if (tempTag != 0 && tempTag != 1) {
-				handleImproperUserInput("Invalid dipeptideOnlyTag (must be 1 or 0)");
-			}
-			dipeptideOnlyTag = (tempTag == 1);
-		}
-		catch (NumberFormatException e) {
-			handleImproperUserInput("Invalid dipeptideOnlyTag (not a number)");
-		}
+    // Load input file into Protein File Manager
+    ProteinManager pManager = null;
+    try {
+      pManager = ProteinManager.load(filepath);
+    }
+    catch (Exception e) {
+      handleImproperUserInput("Invalid input file type");
+    }
 
+    Structure s;
+    try {
+      s = new CifFileReader().getStructure(filepath);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      System.out.println("Failed to load; aborting.");
+      return;
+    }
 
+    DihedralAngle[] angles = DihedralAngle.getDihedrals(args[1]);
+    int idx = Integer.parseInt(args[2]);
+    if (idx >= angles.length) {
+      System.out.println("Can't use angle " + idx + " in residue " + args[1] + "; it doesn't exist.");
+      return;
+    }
+    DihedralAngle angle = angles[idx];
 
-		// Avoid missing "PDB Directory" system property warning
-		System.setProperty("PDB_DIR", pathToInputFileDirectory);
+    BiojavaJmol jmol = new BiojavaJmol();
+    jmol.setStructure(s);
+    jmol.evalString("hide !" + args[1]);
+    jmol.evalString("select *." + angle.a1() + " or *." + angle.a2() + "; color [x00ffff];");
+    for (String atom : angle.rotations())
+      jmol.evalString("select *." + atom + "; color [x44ff44];");
+    for (String atom : angle.clashesWith())
+      jmol.evalString("select *." + atom + "; color [xff4444];");
+    for (String atom : angle.mightClash())
+      jmol.evalString("select *." + atom + "; color [xffff00];");
+  }
 
-
-
-		// Load input file into Protein File Manager
-		ProteinManager pManager = null;
-		try {
-			pManager = ProteinManager.load(filepath);
-		}
-		catch (Exception e) {
-			handleImproperUserInput("Invalid input file type");
-		}
-
-		// Extract structure from loaded file
-		for (Chain chain : pManager.getStructure().getChains()) {
-			for (Group group : chain.getAtomGroups()) {
-				List<Atom> atoms = group.getAtoms();
-
-				// Hardcode bonds for Dummy PDB file
-				if (args[1].equals("DummyCarbons.pdb")) {
-					new BondImpl(atoms.get(0), atoms.get(1),1);
-					new BondImpl(atoms.get(1), atoms.get(2),1);
-					new BondImpl(atoms.get(2), atoms.get(3),1);
-				}
-
-				System.out.println("There are " + atoms.size() + " atoms in " + group.getPDBName());
-				atoms.forEach(a -> System.out.println(a.getName() + " - [ " + a.getBonds() + " ]"));
-				System.out.println();
-
-				Rotator.runSingleResidue(group);
-			}
-		}
-
-		// Output rotated file
-		pManager.export(pathToOutputFileDirectory);
-	}
+  /** 
+   * Alerts the user of improper command line arguments in a consistent, formatted way.
+   * <b>Exits the program.<b>
+   * @param errorMessage the error message to display to the user
+   */
+  private static void handleImproperUserInput(String errorMessage) {
+    System.out.println("\n\033[31m" + "Error: " + errorMessage + "\033[0m");
+    System.out.println("See README for example arguments and proper usage.\n");
+    System.exit(1);
+  }
 }
